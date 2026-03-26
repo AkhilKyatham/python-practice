69723d3f7eb9cf2b73ad3111a85be64b8fb6aba7
akhil kyatham <kyathamakhil9@gmail.com>
2026-03-26 14:28:56 -0500

    Update two_sum.py

--- python/two_sum.py
+++ python/two_sum.py
@@ -1,15 +1,18 @@
 def two_sum(nums, target):
+    """
+    Find indices of two numbers that add up to target.
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    """
     num_map = {}
-    
+
     for i, num in enumerate(nums):
         complement = target - num
-        
+
         if complement in num_map:
             return [num_map[complement], i]
-        
+
         num_map[num] = i
 
     return []
-
-# Example
-print(two_sum([2,7,11,15], 9))
